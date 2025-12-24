5760b6bbf56657f6ee7837c6f4b02bc22131b275
Eser Özbilgiç <you@example.com>
2025-12-24 13:07:44 +0300

    Product List

--- FormsApp/Models/Product.cs
+++ FormsApp/Models/Product.cs
@@ -1,20 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace FormsApp.Models
 {
     public class Product
     {
-        public int Id { get; set; }
-
+        [Display(Name="Product Id")]
+        public int ProductId { get; set; }
+        
+        [Display(Name="Product Name")]
         public string Name { get; set; } = string.Empty;
 
-        public decimal Price { get; set; }
+        [Display(Name="Product Price")]
+        public decimal? Price { get; set; }
 
-        public string Description { get; set; } = string.Empty;
-        
-        public bool isActive { get; set; }
+        [Display(Name="Image")]
+        public string? Image { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
 
-        public int CategoryId { get; set; }
+       
         
-        
+     
+        public int? CategoryId { get; set; }
+
     }
-    
 }
